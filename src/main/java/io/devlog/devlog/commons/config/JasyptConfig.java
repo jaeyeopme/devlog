313396cd0f956b78fae7ac5c95c9ba1352dee2da
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-05-24 19:56:37 +0900

    [#1] - Jasypt 설정

---
+++ src/main/java/io/devlog/devlog/commons/config/JasyptConfig.java
@@ -0,0 +1,34 @@
+package io.devlog.devlog.commons.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    /**
+     * property 암호화를 위한 라이브러리입니다.
+     * https://www.devglan.com/online-tools/jasypt-online-encryption-decryption
+     * 해당 주소에서 시크릿 키를 사용하여 property 를 암호화합니다.
+     * 풀 크기를 서버 머신의 코어 수와 같도록 설정하는 것이 좋습니다.
+     */
+
+    @Bean(name = "jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations(1000);
+        config.setPoolSize(1);
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+
+}
