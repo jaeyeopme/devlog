8b17530286c44e0b3818b1f689ace09d49f8a5af
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-05-26 20:15:14 +0900

    [#2] - memberDto validation 작성

--- src/main/java/io/devlog/devlog/member/dto/MemberDto.java
+++ src/main/java/io/devlog/devlog/member/dto/MemberDto.java
@@ -1,16 +1,29 @@
 package io.devlog.devlog.member.dto;
 
 import io.devlog.devlog.member.domain.entity.Member;
+import lombok.Builder;
 import lombok.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Builder
 @Value
 public class MemberDto {
 
+    @NotBlank
+    @Email(message = "유효하지 않은 이메일 형식입니다.",
+            regexp = "^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$")
     String email;
 
+    @NotBlank
+    @Pattern(message = "최소 한개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야 합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     String password;
 
+    @NotBlank
     String nickname;
 
     public static Member toEntity(MemberDto memberDto, PasswordEncoder passwordEncoder) {
