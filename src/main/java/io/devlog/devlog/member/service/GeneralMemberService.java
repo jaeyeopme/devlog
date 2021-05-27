dafd4c4642d061294acf161322306e05a97cf0c2
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-05-27 17:14:46 +0900

    [#2] - 회원가입 기능 구현 사용자 이메일 중복검사 기능에 메모리 최적화를 위한 readOnly 추가 (FlushMode Manual)

--- src/main/java/io/devlog/devlog/member/service/GeneralMemberService.java
+++ src/main/java/io/devlog/devlog/member/service/GeneralMemberService.java
@@ -1,8 +1,10 @@
 package io.devlog.devlog.member.service;
 
+import io.devlog.devlog.member.domain.entity.Member;
 import io.devlog.devlog.member.domain.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,9 +12,16 @@ public class GeneralMemberService implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     @Override
     public boolean isDuplicatedEmail(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    @Transactional
+    @Override
+    public void registration(Member member) {
+        memberRepository.save(member);
     }
 
 }
--- src/main/java/io/devlog/devlog/member/service/MemberService.java
+++ src/main/java/io/devlog/devlog/member/service/MemberService.java
@@ -1,7 +1,11 @@
 package io.devlog.devlog.member.service;
 
+import io.devlog.devlog.member.domain.entity.Member;
+
 public interface MemberService {
 
     boolean isDuplicatedEmail(String email);
+
+    void registration(Member member);
 
 }
