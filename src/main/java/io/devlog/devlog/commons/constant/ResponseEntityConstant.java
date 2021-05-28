da194461d0e2918b0de0adf81438276b2b1861af
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-05-29 03:30:02 +0900

    [#2] - responseEntityConstant 추가

--- src/main/java/io/devlog/devlog/commons/constant/ResponseEntityConstant.java
+++ src/main/java/io/devlog/devlog/commons/constant/ResponseEntityConstant.java
@@ -1,12 +1,11 @@
-package io.devlog.devlog.commons;
+package io.devlog.devlog.commons.constant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class HttpStatusResponseEntity {
+public class ResponseEntityConstant {
 
     public static ResponseEntity<HttpStatus> RESPONSE_OK = ResponseEntity.status(HttpStatus.OK).build();
-
     public static ResponseEntity<HttpStatus> RESPONSE_CREATED = ResponseEntity.status(HttpStatus.CREATED).build();
 
 }
