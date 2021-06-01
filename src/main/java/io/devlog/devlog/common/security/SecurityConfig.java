2de3751fe016de5b5ffcde87a7d5c114955810e5
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-06-02 04:02:58 +0900

    [#4] - security config 수정

--- src/main/java/io/devlog/devlog/common/security/SecurityConfig.java
+++ src/main/java/io/devlog/devlog/common/security/SecurityConfig.java
@@ -1,20 +1,27 @@
 package io.devlog.devlog.common.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static io.devlog.devlog.user.controller.UserController.USER_API_URI;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,27 +29,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .cors().disable()
                 .headers().frameOptions().disable();
 
-        http
-                .formLogin()
+        http.formLogin()
                 .usernameParameter("email")
                 .loginProcessingUrl(USER_API_URI + "/login")
                 .and()
-                .authorizeRequests()
-                .antMatchers(GET, USER_API_URI + "/duplicated/**")
-                .permitAll()
-                .antMatchers(GET, USER_API_URI + "/verify-token/**")
-                .permitAll()
-                .antMatchers(HttpMethod.POST, USER_API_URI)
-                .permitAll()
-                .antMatchers("/", "/h2-console/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated();
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("SESSION");
+
+        http.authorizeRequests(authorize -> authorize
+                .mvcMatchers(POST, USER_API_URI).anonymous()
+                .mvcMatchers(GET, USER_API_URI + "/verify-token/**").anonymous()
+                .mvcMatchers(GET, USER_API_URI + "/duplicated/**").permitAll()
+                .mvcMatchers("/h2-console/**").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic();
     }
 
 }
