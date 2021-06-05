6447d65d5d2d74d2d3ce1e58c059f4a11cee4886
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-06-05 17:03:48 +0900

    test: user controller 테스트 코드 작성

--- src/test/java/io/devlog/devlog/user/controller/UserControllerTest.java
+++ src/test/java/io/devlog/devlog/user/controller/UserControllerTest.java
@@ -2,174 +2,224 @@ package io.devlog.devlog.user.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.devlog.devlog.common.token.EmailTokenService;
+import io.devlog.devlog.common.email.EmailTokenService;
+import io.devlog.devlog.user.domain.entity.PrincipalDetails;
 import io.devlog.devlog.user.domain.entity.User;
 import io.devlog.devlog.user.dto.UserRequest;
+import io.devlog.devlog.user.dto.UserResponse;
 import io.devlog.devlog.user.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
-import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 
-import static io.devlog.devlog.docs.ApiDocumentationUtil.getDocumentRequest;
-import static io.devlog.devlog.docs.ApiDocumentationUtil.getDocumentResponse;
-import static io.devlog.devlog.fixture.UserFixture.*;
 import static io.devlog.devlog.user.controller.UserController.USER_API_URI;
 import static io.devlog.devlog.user.exception.UserResponseStatusException.INVALID_TOKEN_EXCEPTION;
+import static io.devlog.devlog.user.exception.UserResponseStatusException.USER_NOT_FOUND_EXCEPTION;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureRestDocs
-@ExtendWith(RestDocumentationExtension.class)
 @WebMvcTest(UserController.class)
 class UserControllerTest {
 
+    private final Long userId = 1L;
+    private final String emailToken = "emailToken";
     @MockBean
     UserService userService;
-
     @MockBean
     EmailTokenService emailTokenService;
-
     @MockBean
     PasswordEncoder passwordEncoder;
-
     @Autowired
     MockMvc mockMvc;
-
     @Autowired
     ObjectMapper objectMapper;
+    private User user;
+    private RequestPostProcessor principal;
+    private UserRequest userRequest;
+    private String userRequestContent;
+    private String userResponseContent;
 
-    private String toJsonString(Object object) throws JsonProcessingException {
-        return objectMapper.writeValueAsString(object);
+    @BeforeEach
+    void setUp() throws JsonProcessingException {
+        userRequest = UserRequest.builder()
+                .email("email@email.com")
+                .password("Password1234!")
+                .nickname("nickname")
+                .build();
+
+        user = UserRequest.toEntity(userRequest, passwordEncoder);
+
+        principal = SecurityMockMvcRequestPostProcessors.user(new PrincipalDetails(user));
+
+        userRequestContent = objectMapper.writeValueAsString(userRequest);
+
+        userResponseContent = objectMapper.writeValueAsString(UserResponse.of(user));
     }
 
+    @DisplayName("중복되지 않은 이메일로 회원가입한 경우 HTTP 상태코드 201을 반환한다.")
     @Test
-    @DisplayName("회원가입에 성공한다.")
-    void registration_Success() throws Exception {
-        User user = UserRequest.toEntity(USER_REQUEST, passwordEncoder);
+    void registerWithNotDuplicatedEmail() throws Exception {
+        given(userService.isDuplicated(userRequest.getEmail())).willReturn(false);
         willDoNothing().given(userService).register(user);
+        willDoNothing().given(emailTokenService).sendEmailToken(userRequest.getEmail());
 
-        mockMvc.perform(post(USER_API_URI)
+        MockHttpServletRequestBuilder requestBuilder = post(USER_API_URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(toJsonString(USER_REQUEST)))
+                .content(userRequestContent);
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
-                .andExpect(status().isCreated())
-                .andDo(document("users/create/success",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        requestFields(
-                                fieldWithPath("email").type(JsonFieldType.STRING)
-                                        .description("로그인시 사용할 사용자 이메일"),
-                                fieldWithPath("password").type(JsonFieldType.STRING)
-                                        .description("하나 이상의 대소문자, 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호"),
-                                fieldWithPath("nickname").type(JsonFieldType.STRING)
-                                        .description("사용자의 닉네임")
-                        )
-                ));
+                .andExpect(status().isCreated());
     }
 
+    @DisplayName("중복된 이메일로 회원가입한 경우 HTTP 상태코드 409를 반환한다.")
     @Test
-    @DisplayName("중복된 이메일로 회원가입에 실패한다.")
-    void registration_Fail() throws Exception {
-        given(userService.isDuplicated(USER_REQUEST.getEmail())).willReturn(true);
+    void registerWithDuplicatedEmail() throws Exception {
+        given(userService.isDuplicated(userRequest.getEmail())).willReturn(true);
 
-        mockMvc.perform(post(USER_API_URI)
+        MockHttpServletRequestBuilder requestBuilder = post(USER_API_URI)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(toJsonString(USER_REQUEST)))
+                .content(userRequestContent);
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
                 .andExpect(status().isConflict())
-                .andExpect(status().reason("중복된 이메일 입니다."))
-                .andDo(document("users/create/fail",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        requestFields(
-                                fieldWithPath("email").type(JsonFieldType.STRING)
-                                        .description("로그인시 사용할 사용자 이메일"),
-                                fieldWithPath("password").type(JsonFieldType.STRING)
-                                        .description("하나 이상의 대소문자, 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호"),
-                                fieldWithPath("nickname").type(JsonFieldType.STRING)
-                                        .description("사용자의 닉네임")
-                        )
-                ));
+                .andExpect(status().reason("중복된 이메일 입니다."));
     }
 
+    @DisplayName("회원가입 되어있는 사용자를 조회할 경우 HTTP 상태코드 200을 반환한다.")
     @Test
-    @DisplayName("이메일 중복 검사 - 사용 가능한 이메일")
-    void idDuplicatedEmail_Success() throws Exception {
-        given(userService.isDuplicated(USER_REQUEST.getEmail())).willReturn(false);
+    void searchWithExistUser() throws Exception {
+        given(userService.findById(userId)).willReturn(user);
 
-        mockMvc.perform(RestDocumentationRequestBuilders.get(USER_API_URI + "/duplicated/{email}", USER_REQUEST.getEmail()))
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/{id}", userId)
+                .with(principal);
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andDo(document("users/duplicateVerification/success",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("email").description("사용 가능한 이메일"))));
+                .andExpect(content().json(userResponseContent));
     }
 
+    @DisplayName("회원가입 되어있지 않은 사용자를 조회할 경우 HTTP 상태코드 404와 메시지를 반환한다.")
     @Test
-    @DisplayName("이메일 중복 검사 - 중복된 이메일")
-    void idDuplicatedEmail_Fail() throws Exception {
-        given(userService.isDuplicated(USER_REQUEST.getEmail())).willReturn(true);
+    void searchWithNotExistUser() throws Exception {
+        given(userService.findById(userId)).willThrow(USER_NOT_FOUND_EXCEPTION);
 
-        mockMvc.perform(RestDocumentationRequestBuilders.get(USER_API_URI + "/duplicated/{email}", USER_REQUEST.getEmail()))
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/{id}", userId)
+                .with(principal);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(status().reason("사용자를 찾을 수 없습니다."));
+    }
+
+    @DisplayName("접속되어있는 사용자가 개인 프로필을 조회할 경우 HTTP 상태코드 200과 UserResponse 를 반환한다.")
+    @Test
+    void getProfile() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/my-profile")
+                .with(principal);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(userResponseContent));
+    }
+
+    @DisplayName("접속되어있는 사용자가 개인 프로필을 수정할 경우 HTTP 상태코드 200과 UserResponse 를 반환한다.")
+    @Test
+    void updateProfile() throws Exception {
+        willDoNothing().given(userService).updateUserProfile(user, userRequest);
+
+        MockHttpServletRequestBuilder requestBuilder = put(USER_API_URI)
+                .with(principal)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userRequestContent);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(userResponseContent));
+    }
+
+    @DisplayName("중복된 이메일이 없을 경우 HTTP 상태코드 200을 반환한다.")
+    @Test
+    void validateWithNotDuplicatedEmail() throws Exception {
+        given(userService.isDuplicated(userRequest.getEmail())).willReturn(false);
+
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/duplicate/{email}", userRequest.getEmail());
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("이메일이 중복되었을 경우 HTTP 상태코드 409와 메시지를 반환한다.")
+    @Test
+    void validateWithDuplicatedEmail() throws Exception {
+        given(userService.isDuplicated(userRequest.getEmail())).willReturn(true);
+
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/duplicate/{email}", userRequest.getEmail());
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
                 .andExpect(status().isConflict())
-                .andExpect(status().reason("중복된 이메일 입니다."))
-                .andDo(document("users/duplicateVerification/fail",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("email").description("중복된 이메일"))));
+                .andExpect(status().reason("중복된 이메일 입니다."));
     }
 
-    @Test
-    @DisplayName("이메일 토큰 검증 - 유효한 토큰이라면 인증에 성공한다.")
-    void verifyEmailToken_Success() throws Exception {
-        given(emailTokenService.verify(EMAIL_TOKEN)).willReturn(USER.getEmail());
 
-        mockMvc.perform(RestDocumentationRequestBuilders.get(USER_API_URI + "/verify-token/{token}", EMAIL_TOKEN))
+    @DisplayName("이메일 토큰 전송에 성공한 경우 HTTP 상태코드 200을 반환한다.")
+    @Test
+    void sendEmailTokenWithLoginUser() throws Exception {
+        willDoNothing().given(emailTokenService).sendEmailToken(userRequest.getEmail());
+
+        MockHttpServletRequestBuilder requestBuilder = post(USER_API_URI + "/email-verification")
+                .with(principal);
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andDo(document("users/tokenVerification/success",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("token").description("유효한 토큰"))));
+                .andExpect(status().isOk());
     }
 
+    @DisplayName("유효한 이메일 토큰일 경우 HTTP 상태코드 200을 반환한다.")
     @Test
-    @DisplayName("이메일 토큰 검증 - 유효하지 않은 토큰이라면 인증에 실패한다.")
-    void verifyEmailToken_Fail() throws Exception {
-        given(emailTokenService.verify(EMAIL_TOKEN)).willThrow(INVALID_TOKEN_EXCEPTION);
+    void verifyWithValidEmailToken() throws Exception {
+        given(emailTokenService.verify(emailToken)).willReturn(userRequest.getEmail());
 
-        mockMvc.perform(RestDocumentationRequestBuilders.get(USER_API_URI + "/verify-token/{token}", EMAIL_TOKEN))
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/verify-token/{token}", emailToken);
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("유효하지 않은 이메일 토큰일 경우 HTTP 상태코드 401과 메시지를 반환한다.")
+    @Test
+    void verifyWithInvalidEmailToken() throws Exception {
+        given(emailTokenService.verify(emailToken)).willThrow(INVALID_TOKEN_EXCEPTION);
+
+        MockHttpServletRequestBuilder requestBuilder = get(USER_API_URI + "/verify-token/{token}", emailToken);
+
+        mockMvc.perform(requestBuilder)
                 .andDo(print())
                 .andExpect(status().isUnauthorized())
-                .andExpect(status().reason("인증토큰이 만료되었습니다."))
-                .andDo(document("users/tokenVerification/fail",
-                        getDocumentRequest(),
-                        getDocumentResponse(),
-                        pathParameters(
-                                parameterWithName("token").description("유효하지 않은 토큰"))));
+                .andExpect(status().reason("인증토큰이 만료되었습니다."));
     }
 
 }
