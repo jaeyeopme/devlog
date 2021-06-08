ae93eb47e53f57c69f2a40d91e2fd5c266603f71
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-06-08 22:59:03 +0900

    style: 누락된 기능 삭제

--- src/main/java/io/devlog/devlog/user/domain/repository/UserRepository.java
+++ src/main/java/io/devlog/devlog/user/domain/repository/UserRepository.java
@@ -2,9 +2,6 @@ package io.devlog.devlog.user.domain.repository;
 
 import io.devlog.devlog.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -17,9 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User save(User user);
 
     Optional<User> findByEmail(String email);
-
-    @Modifying
-    @Query(value = "UPDATE User u SET u.enabled = true WHERE u.email= :email")
-    void setEnabledByEmail(@Param("email") String email);
 
 }
