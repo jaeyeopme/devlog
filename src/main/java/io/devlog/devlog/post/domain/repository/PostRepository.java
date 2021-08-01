913eb6ba431697d8f9d1047427ef4eda2c53f53b
Jaeyeop Lee <jaeyeop.dev@gmail.com>
2021-08-01 22:44:45 +0900

    refactor: @Repository 주석 제거 JpaRepository 의 기본 구현체 SimpleJpaRepository 참조

--- src/main/java/io/devlog/devlog/post/domain/repository/PostRepository.java
+++ src/main/java/io/devlog/devlog/post/domain/repository/PostRepository.java
@@ -2,9 +2,7 @@ package io.devlog.devlog.post.domain.repository;
 
 import io.devlog.devlog.post.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
